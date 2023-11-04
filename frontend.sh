0ede7db3226a74d888d9a09828c00cb84eda990e
LOKESH <swarnaloke2095@gmail.com>
2023-11-04 14:10:00 +0530

    sample code

--- frontend.sh
+++ frontend.sh
@@ -1,15 +1,15 @@
 echo -e "\e[31m installing nginx \e[0m"
 dnf install nginx -y
-
+echo -e "\e[33m copying expense.conf \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
-
+echo -e "\e[32m removing old content \e[0m"
 rm -rf /usr/share/nginx/html/*
-
+echo -e "\e[35m downloading new file \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
-
+echo -e "\e[36m unzipping \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-
+echo -e "\e[31m starting service \e[0m"
 systemctl enable nginx
 systemctl restart nginx
 
