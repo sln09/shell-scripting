e0827e84ecadb9ac1c92107fc43dff707195db0c
LOKESH <swarnaloke2095@gmail.com>
2023-11-04 19:20:50 +0530

    sample code

--- frontend.sh
+++ frontend.sh
@@ -10,17 +10,33 @@ else
 fi
 echo -e "$color copying expense.conf \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log-file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[33m success \e[0m"
+else
+    echo -e "\e[31m fail \e[0m"
+fi
 echo -e "$color removing old content \e[0m"
 rm -rf /usr/share/nginx/html/* &>>$log-file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[33m success \e[0m"
+else
+    echo -e "\e[31m fail \e[0m"
+fi
 echo -e "$color downloading new file \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>$log-file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e "\e[33m success \e[0m"
+else
+    echo -e "\e[31m fail \e[0m"
+fi
 echo -e "$color unzipping \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$log-file
-echo $?
+if [ $? -eq 0 ]; then
+  echo -e"\e[33m success \e[0m"
+else
+    echo -e"\e[31m fail \e[0m"
+fi
 echo -e "$color starting service \e[0m"
 systemctl enable nginx &>>$log-file
 systemctl restart nginx &>>$log-file
